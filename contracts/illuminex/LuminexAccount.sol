50cab635325a741cb27df92693279fc4e18f5d6f
Berklium Master <berklium-birb@protonmail.com>
2024-04-29 17:51:53 +0200

    fix: Type imports

--- contracts/illuminex/LuminexAccount.sol
+++ contracts/illuminex/LuminexAccount.sol
@@ -8,7 +8,6 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
--- contracts/illuminex/LuminexFeeCalculator.sol
+++ contracts/illuminex/LuminexFeeCalculator.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 abstract contract LuminexFeeCalculator is Ownable {
 
