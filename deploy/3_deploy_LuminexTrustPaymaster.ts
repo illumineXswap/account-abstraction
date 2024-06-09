65b8a8ce61efe31b8044425565bc1c747d40ca6b
party-for-illuminati <cattykittycat@proton.me>
2024-06-09 23:21:04 +0500

    feat: Add approveCalls to LuminexPaymaster deployment script

--- deploy/3_deploy_LuminexTrustPaymaster.ts
+++ deploy/3_deploy_LuminexTrustPaymaster.ts
@@ -3,6 +3,59 @@ import { DeployFunction } from 'hardhat-deploy/types'
 import { ethers } from 'hardhat'
 import assert from 'node:assert'
 import { WNATIVE } from './const'
+import { LuminexAccountFactory, LuminexTokenPaymaster } from '../typechain'
+import * as fs from 'node:fs'
+
+const approveCalls = async (factory: LuminexAccountFactory, paymaster: LuminexTokenPaymaster): Promise<void> => {
+  const selector = (abiString: string): string => {
+    return ethers.utils.keccak256(abiString).slice(10)
+  }
+
+  const foreignTargetAllowedCalls = {
+    '0xaA12E7aB0Ed8dd1814979bc6bd21E2aC17CD2F0E': [
+      selector('getHeldTokens(uint256,uint256)')
+    ],
+    '0xDd1Ee07b46C7eD888671a646F3c7a37394e8cF85': [
+      selector('proxyPass(address,uint256,bytes)')
+    ]
+  }
+
+  const getSelectorsWhitelist = (): Record<string, string[]> => {
+    const paymasterTargetAllowedCalls = {
+      [paymaster.address]: [
+        selector('buyNativeForToken(address,uint256,uint256,address)'),
+        selector('tokensRequiredForNative(address,uint256)')
+      ]
+    }
+
+    const tokensData = JSON.parse(fs.readFileSync('./tokens.json', 'utf-8'))
+
+    const tokensAllowedCalls = Object.fromEntries(
+      tokensData.tokens
+        .filter((token: any) => token.extensions?.illuminexWrapper)
+        .map((token: any) => ([
+          token.address,
+          [
+            selector('transfer(address,uint256)'),
+            selector('approve(address,uint256)'),
+            selector('transferFrom(address,address,uint256)'),
+            selector('balanceOf(address)')
+          ]
+        ]))
+    )
+
+    return {
+      ...foreignTargetAllowedCalls,
+      ...paymasterTargetAllowedCalls,
+      ...tokensAllowedCalls
+    }
+  }
+
+  const allowedCalls = getSelectorsWhitelist()
+  for (const [target, selectors] of Object.entries(allowedCalls)) {
+    await factory.allowCalls(target, selectors)
+  }
+}
 
 const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider
@@ -21,6 +74,13 @@ const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatR
       log: true,
       deterministicDeployment: true
     })
+
+  const factoryDeployed = await hre.deployments.get('LuminexAccountFactory')
+
+  const factory = await hre.ethers.getContractAt('LuminexAccountFactory', factoryDeployed.address)
+  const paymaster = await hre.ethers.getContractAt('LuminexTokenPaymaster', paymasterDeployed.address)
+
+  await approveCalls(factory, paymaster)
 
   const entryPoint = await ethers.getContractAt('EntryPoint', entrypointDeployment.address)
 
