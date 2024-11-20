f1e1f06f5609bcf85ac218f86b54d8336d05e915
party-for-illuminati <cattykittycat@proton.me>
2024-11-20 16:25:18 +0100

    feat: Add WBTC

--- deploy/4_deploy_testtoken.ts
+++ deploy/4_deploy_testtoken.ts
@@ -57,6 +57,7 @@ const deploySimpleAccountFactory: DeployFunction = async (hre: HardhatRuntimeEnv
     ['ETH@arbitrum', '36812.04274074493'],
     ['POL', '6.505047202855522'],
     ['BTC', '878354.127267101'],
+    ['WBTC@eth', '878354.127267101']
   ])
 
   const txs = []
